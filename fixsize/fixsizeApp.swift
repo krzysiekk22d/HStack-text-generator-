a59239199f160ef995a2cbe6ba392dd7cf59ab1f
Krzysztof Czura <krzczu5@gmail.com>
2024-02-09 22:50:48 +0100

    Initial Commit

---
+++ fixsize/fixsizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fixsizeApp.swift
+//  fixsize
+//
+//  Created by Krzysztof Czura on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct fixsizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
