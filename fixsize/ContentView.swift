b8920b7cc73c76105af170738dcde0df7633a243
Krzysztof Czura <krzczu5@gmail.com>
2024-02-10 18:27:45 +0100

    Implement a logic for words.

--- fixsize/ContentView.swift
+++ fixsize/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var elements: Array<String> = ["J", "e", "s", "t", "e", "m", " ", "p", "r", "o", "g", "r", "a", "m", "e", "m", " ", "k", "o", "m", "p", "u", "t", "e", "r", "o", "w", "y", "m", ", ", " ", "g", "o", "t", "o", "w", "y", "m", " ", "d", "o", " ", "p", "o", "m", "o", "c", "y", " ", "w", " ", "d", "o", "w", "o", "l", "n", "y", "c", "h", " ", "z", "a", "d", "a", "n", "i", "a", "c", "h", " ", "p", "r", "o", "g", "r", "a", "m", "i", "s", "t", "y", "c", "z", "n", "y", "c", "h", "."]
+    @State private var elements: Array<String> = ["J", "e", "s", "t", "e", "m", " ", "p", "r", "o", "g", "r", "a", "m", "e", "m", " ", "k", "o", "m", "p", "u", "t", "e", "r", "o", "w", "y", "m", ",", " ", "g", "o", "t", "o", "w", "y", "m", " ", "d", "o", " ", "p", "o", "m", "o", "c", "y", " ", "w", " ", "d", "o", "w", "o", "l", "n", "y", "c", "h", " ", "z", "a", "d", "a", "n", "i", "a", "c", "h", " ", "p", "r", "o", "g", "r", "a", "m", "i", "s", "t", "y", "c", "z", "n", "y", "c", "h", "."]
 
     
     var body: some View {
@@ -39,31 +39,42 @@ struct HStacksContainer: View {
         var lines: [[String]] = [[]]
         var currentLineIndex = 0
         var remainingWidth: CGFloat = UIScreen.main.bounds.width - 50
-        var currentWord: [String] = []
+        var currentWord: String = ""
+        var words: [String] = []
         
-        for element in elements {
-            let elementWidth = element.width(withConstrainedHeight: 1000, font: .systemFont(ofSize: 18)) // Assuming font size 18
-            
+        // Build words
+        for (index, element) in elements.enumerated() {
             if element == " " {
-                // Add current word to the line, reset currentWord
-                lines[currentLineIndex] += currentWord + [element]
-                currentWord = []
-            } else if elementWidth > remainingWidth {
-                // Move to the next line for non-space elements that don't fit
-                lines.append(currentWord + [element])
-                currentWord = []
-                currentLineIndex += 1
-                remainingWidth = UIScreen.main.bounds.width - elementWidth - 50
+                currentWord.append(element)
+                words.append(currentWord)
+                currentWord = ""
             } else {
                 // Add the element to the current word
                 currentWord.append(element)
-                remainingWidth -= elementWidth
+                
+                // Check if this is the last element
+                if index == elements.count - 1 {
+                    words.append(currentWord)
+                }
             }
         }
         
-        // Add the last word to the last line
-        lines[currentLineIndex] += currentWord
+        for word in words {
+            let wordWidth = word.width(withConstrainedHeight: 1000, font: .systemFont(ofSize: 18)) // Assuming font size 18
+            
+            if wordWidth > remainingWidth {
+                currentLineIndex += 1
+                lines.append([word])
+                remainingWidth = UIScreen.main.bounds.width - wordWidth - 50
+            } else {
+                lines[currentLineIndex].append(word)
+                remainingWidth -= wordWidth
+            }
+        }
         
+        print("lines: \(lines)")
+//        print("screen: \(UIScreen.main.bounds.maxX)")
+        // napis się psuje jak się ekran odwraca
         return lines
     }
 }
